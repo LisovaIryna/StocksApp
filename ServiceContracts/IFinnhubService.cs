17f9c55e3d42fed0a40f673d8bd62b2c84efefe1
LisovaIryna <79648928+LisovaIryna@users.noreply.github.com>
2025-07-20 19:02:17 +0300

    Add IFinnhubService that represents service, which makes HTTP requests to finnhub.io

---
+++ ServiceContracts/IFinnhubService.cs
@@ -0,0 +1,21 @@
+﻿namespace ServiceContracts;
+
+/// <summary>
+/// Represents service that makes HTTP requests to finnhub.io
+/// </summary>
+public interface IFinnhubService
+{
+    /// <summary>
+    /// Returns company details
+    /// </summary>
+    /// <param name="stockSymbol">Stock symbol to search</param>
+    /// <returns>Dictionary that contains company details</returns>
+    Task<Dictionary<string, object>?> GetCompanyProfile(string stockSymbol);
+
+    /// <summary>
+    /// Returns stock price details
+    /// </summary>
+    /// <param name="stockSymbol">Stock symbol to search</param>
+    /// <returns>Dictionary that contains stock price details</returns>
+    Task<Dictionary<string, object>?> GetStockPriceQuote(string stockSymbol);
+}
