c60c545bebf30dbc491a2c72c9aedf93c0790eee
LisovaIryna <79648928+LisovaIryna@users.noreply.github.com>
2025-07-20 13:21:19 +0300

    Create Controllers folder with TradeController

---
+++ StocksApp/Controllers/TradeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StocksApp.Controllers;
+
+public class TradeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
