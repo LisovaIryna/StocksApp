7d5c912a00baa8d162ed4c7964005c8da76a3fc7
LisovaIryna <79648928+LisovaIryna@users.noreply.github.com>
2025-07-20 19:43:23 +0300

    Add IStocksService

---
+++ ServiceContracts/IStocksService.cs
@@ -0,0 +1,28 @@
+﻿namespace ServiceContracts;
+
+public interface IStocksService
+{
+    /// <summary>
+    /// Creates a buy order
+    /// </summary>
+    /// <param name="buyOrderRequest">Buy order object</param>
+    Task<BuyOrderResponse> CreateBuyOrder(BuyOrderRequest? buyOrderRequest);
+
+    /// <summary>
+    /// Creates a sell order
+    /// </summary>
+    /// <param name="buyOrderRequest">Sell order object</param>
+    Task<SellOrderResponse> CreateSellOrder(SellOrderRequest? buyOrderRequest);
+
+    /// <summary>
+    /// Returns all existing buy orders
+    /// </summary>
+    /// <returns>Returns a list of object of BuyOrder type</returns>
+    Task<List<BuyOrderResponse>> GetBuyOrders();
+
+    /// <summary>
+    /// Returns all existing sell orders
+    /// </summary>
+    /// <returns>Returns a list of objects of SellOrder type</returns>
+    Task<List<SellOrderResponse>> GetSellOrders();
+}
