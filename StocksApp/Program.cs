75f0c7467d097c1cf1ce51d615cf69a186474fc8
LisovaIryna <79648928+LisovaIryna@users.noreply.github.com>
2025-07-20 14:45:15 +0300

    Create a configuration as service using Options pattern

--- StocksApp/Program.cs
+++ StocksApp/Program.cs
@@ -1,5 +1,8 @@
+using StocksApp;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+builder.Services.Configure<TradingOptions>(builder.Configuration.GetSection("TradingOptions")); 
 
 var app = builder.Build();
 app.UseStaticFiles();
