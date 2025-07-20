a2289548e1a22d72a0ff4da05f2f8a73afbfaded
LisovaIryna <79648928+LisovaIryna@users.noreply.github.com>
2025-07-20 16:02:53 +0300

    Inject the IOptions in the controller

--- StocksApp/Controllers/TradeController.cs
+++ StocksApp/Controllers/TradeController.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace StocksApp.Controllers;
 
 public class TradeController : Controller
 {
+    private readonly TradingOptions _tradingOptions;
+
+    public TradeController(IOptions<TradingOptions> tradingOptions)
+    {
+        _tradingOptions = tradingOptions.Value;
+    }
+
     public IActionResult Index()
     {
         return View();
