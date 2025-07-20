f28d8a4aa5025e9057512970852618905f8f7c2a
LisovaIryna <79648928+LisovaIryna@users.noreply.github.com>
2025-07-20 20:04:58 +0300

    Inject IStocksService and IFinnhubService in controller

--- StocksApp/Controllers/TradeController.cs
+++ StocksApp/Controllers/TradeController.cs
@@ -1,15 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using ServiceContracts;
 
 namespace StocksApp.Controllers;
 
 public class TradeController : Controller
 {
     private readonly TradingOptions _tradingOptions;
+    private readonly IStocksService _stocksService;
+    private readonly IFinnhubService _finnhubService;
+    private readonly IConfiguration _configuration;
 
-    public TradeController(IOptions<TradingOptions> tradingOptions)
+    public TradeController(IOptions<TradingOptions> tradingOptions, IStocksService stocksService, IFinnhubService finnhubService, IConfiguration configuration)
     {
         _tradingOptions = tradingOptions.Value;
+        _stocksService = stocksService;
+        _finnhubService = finnhubService;
+        _configuration = configuration;
     }
 
     public IActionResult Index()
