8087082d8f64047eb6089c46a56d33010eb6308f
LisovaIryna <79648928+LisovaIryna@users.noreply.github.com>
2025-07-21 15:30:23 +0300

    Implement FinnhubService and StocksService

--- Services/FinnhubService.cs
+++ Services/FinnhubService.cs
@@ -1,6 +1,69 @@
-﻿namespace Services;
+﻿using Microsoft.Extensions.Configuration;
+using ServiceContracts;
+using System.Text.Json;
 
-public class FinnhubService
+namespace Services;
+
+public class FinnhubService : IFinnhubService
 {
+    private readonly IHttpClientFactory _httpClientFactory;
+    private readonly IConfiguration _configuration;
 
+    public FinnhubService(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+    {
+        _httpClientFactory = httpClientFactory;
+        _configuration = configuration;
+    }
+
+    public async Task<Dictionary<string, object>?> GetCompanyProfile(string stockSymbol)
+    {
+        HttpClient httpClient = _httpClientFactory.CreateClient();
+
+        HttpRequestMessage httpRequestMessage = new()
+        {
+            RequestUri = new Uri($"https://finnhub.io/api/v1/stock/profile2?symbol={stockSymbol}&token={_configuration["FinnhubToken"]}"),
+            Method = HttpMethod.Get
+        };
+
+        HttpResponseMessage httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
+
+        StreamReader streamReader = new(httpResponseMessage.Content.ReadAsStream());
+        string response = streamReader.ReadToEnd();
+
+        Dictionary<string, object>? responseDictionary = JsonSerializer.Deserialize<Dictionary<string, object>>(response);
+
+        if (responseDictionary == null)
+            throw new InvalidOperationException("No response from finnhub server");
+
+        if (responseDictionary.ContainsKey("error"))
+            throw new InvalidOperationException(Convert.ToString(responseDictionary["error"]));
+
+        return responseDictionary;
+    }
+
+    public async Task<Dictionary<string, object>?> GetStockPriceQuote(string stockSymbol)
+    {
+        HttpClient httpClient = _httpClientFactory.CreateClient();
+
+        HttpRequestMessage httpRequestMessage = new()
+        {
+            RequestUri = new Uri($"https://finnhub.io/api/v1/quote?symbol={stockSymbol}&token={_configuration["FinnhubToken"]}"),
+            Method = HttpMethod.Get
+        };
+
+        HttpResponseMessage httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
+
+        StreamReader streamReader = new(httpResponseMessage.Content.ReadAsStream());
+        string response = streamReader.ReadToEnd();
+
+        Dictionary<string, object>? responseDictionary = JsonSerializer.Deserialize<Dictionary<string, object>>(response);
+
+        if (responseDictionary == null)
+            throw new InvalidOperationException("No response from finnhub service");
+
+        if (responseDictionary.ContainsKey("error"))
+            throw new InvalidOperationException(Convert.ToString(responseDictionary["error"]));
+
+        return responseDictionary;
+    }
 }
--- Services/StocksService.cs
+++ Services/StocksService.cs
@@ -1,12 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ServiceContracts.DTO;
+using Services.Helpers;
 
-namespace Services
+namespace Services;
+
+public class StocksService : IStocksService
 {
-    internal class StocksService
+    private readonly List<BuyOrder> _buyOrders;
+    private readonly List<SellOrder> _sellOrders;
+
+    public StocksService()
     {
+        _buyOrders = new();
+        _sellOrders = new();
+    }
+
+    public Task<BuyOrderResponse> CreateBuyOrder(BuyOrderRequest? buyOrderRequest)
+    {
+        if (buyOrderRequest == null)
+            throw new ArgumentNullException(nameof(buyOrderRequest));
+
+        ValidationHelper.ModelValidation(buyOrderRequest);
+
+        BuyOrder buyOrder = buyOrderRequest.ToBuyOrder();
+
+        buyOrder.BuyOrderID = Guid.NewGuid();
+
+        _buyOrders.Add(buyOrder);
+
+        return Task.FromResult(buyOrder.ToBuyOrderResponse());
+    }
+
+    public Task<SellOrderResponse> CreateSellOrder(SellOrderRequest? sellOrderRequest)
+    {
+        if (sellOrderRequest == null)
+            throw new ArgumentNullException(nameof(sellOrderRequest));
+
+        ValidationHelper.ModelValidation(sellOrderRequest);
+
+        SellOrder sellOrder = sellOrderRequest.ToSellOrder();
+
+        sellOrder.SellOrderID = Guid.NewGuid();
+
+        _sellOrders.Add(sellOrder);
+
+        return Task.FromResult(sellOrder.ToSellOrderResponse());
+    }
+
+    public Task<List<BuyOrderResponse>> GetBuyOrders()
+    {
+        return Task.FromResult(_buyOrders
+            .OrderByDescending(temp => temp.DateAndTimeOfOrder)
+            .Select(temp => temp.ToBuyOrderResponse()).ToList());
+    }
+
+    public Task<List<SellOrderResponse>> GetSellOrders()
+    {
+        return Task.FromResult(_sellOrders
+            .OrderByDescending(temp => temp.DateAndTimeOfOrder)
+            .Select(temp => temp.ToSellOrderResponse()).ToList());
     }
 }
