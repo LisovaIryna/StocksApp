caea65c13f814f28c14f5348bbc342f08c84a9bc
LisovaIryna <79648928+LisovaIryna@users.noreply.github.com>
2025-10-13 19:27:21 +0300

    Add GetStocks and SearchStocks methods to IFinnhubService

--- ServiceContracts/IFinnhubService.cs
+++ ServiceContracts/IFinnhubService.cs
@@ -18,4 +18,17 @@ public interface IFinnhubService
     /// <param name="stockSymbol">Stock symbol to search</param>
     /// <returns>Dictionary that contains stock price details</returns>
     Task<Dictionary<string, object>?> GetStockPriceQuote(string stockSymbol);
+
+    /// <summary>
+    /// Returns list of all stocks supported by an exchange
+    /// </summary>
+    /// <returns>List of stocks</returns>
+    Task<List<Dictionary<string, string>>?> GetStocks();
+
+    /// <summary>
+    /// Returns list of matching stocks based on the given stock symbol
+    /// </summary>
+    /// <param name="stockSymbolToSearch">Stock symbol to search</param>
+    /// <returns>List of matching stocks</returns>
+    Task<Dictionary<string, object>?> SearchStocks(string stockSymbolToSearch);
 }
