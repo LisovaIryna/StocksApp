5eeda0a971b3634e7e65d097322fdf5c88410a83
LisovaIryna <79648928+LisovaIryna@users.noreply.github.com>
2025-10-18 15:33:04 +0300

    Create StocksController

---
+++ StocksApp/Controllers/StocksController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using ServiceContracts;
+
+namespace StocksApp.Controllers;
+
+[Route("[controller]")]
+public class StocksController : Controller
+{
+    private readonly TradingOptions _tradingOptions;
+    private readonly IFinnhubService _finnhubService;
+
+    public StocksController(IOptions<TradingOptions> tradingOptions, IFinnhubService finnhubService)
+    {
+        _tradingOptions = tradingOptions.Value;
+        _finnhubService = finnhubService;
+    }
+}
