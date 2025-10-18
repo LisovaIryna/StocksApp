3fa2ad6fa1885a3e7592411f4d1c6aef4a4b0ecd
LisovaIryna <79648928+LisovaIryna@users.noreply.github.com>
2025-10-18 17:40:43 +0300

    Add Explore method to StocksController

--- StocksApp/Controllers/StocksController.cs
+++ StocksApp/Controllers/StocksController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using ServiceContracts;
+using StocksApp.Models;
 
 namespace StocksApp.Controllers;
 
@@ -14,5 +15,38 @@ public class StocksController : Controller
     {
         _tradingOptions = tradingOptions.Value;
         _finnhubService = finnhubService;
+    }
+
+    [Route("/")]
+    [Route("[action]/{stock?}")]
+    [Route("~/[action]/{stock?}")]
+    public async Task<IActionResult> Explore(string? stock, bool showAll = false)
+    {
+        List<Dictionary<string, string>>? stocksDictionary = await _finnhubService.GetStocks();
+        List<Stock> stocks = new();
+
+        if (stocksDictionary is not null)
+        {
+            if (!showAll && _tradingOptions.Top25PopularStocks != null)
+            {
+                string[]? Top25PopularStocksList = _tradingOptions.Top25PopularStocks.Split(",");
+                if (Top25PopularStocksList is not null)
+                {
+                    stocksDictionary = stocksDictionary
+                        .Where(temp => Top25PopularStocksList.Contains(Convert.ToString(temp["symbol"]))).ToList();
+                }
+            }
+
+            stocks = stocksDictionary
+                .Select(temp => new Stock()
+                {
+                    StockName = Convert.ToString(temp["description"]),
+                    StockSymbol = Convert.ToString(temp["symbol"])
+                }).ToList();
+        }
+
+        ViewBag.Stock = stock;
+
+        return View(stocks);
     }
 }
