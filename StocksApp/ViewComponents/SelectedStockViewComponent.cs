18604001fc5f188c5e6c94b7a44b85a6bd773bb7
LisovaIryna <79648928+LisovaIryna@users.noreply.github.com>
2025-10-18 20:14:50 +0300

    Create SelectedStockViewComponent

---
+++ StocksApp/ViewComponents/SelectedStockViewComponent.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceContracts;
+using ServiceContracts.DTO;
+using Microsoft.Extensions.Options;
+
+namespace StocksApp.ViewComponents;
+
+public class SelectedStockViewComponent :ViewComponent
+{
+    private readonly TradingOptions _tradingOptions;
+    private readonly IStocksService _stocksService;
+    private readonly IFinnhubService _finnhubService;
+    private readonly IConfiguration _configuration;
+
+    public SelectedStockViewComponent (IOptions<TradingOptions> tradingOptions, IStocksService stocksService, IFinnhubService finnhubService, IConfiguration configuration)
+    {
+        _tradingOptions = tradingOptions.Value;
+        _stocksService = stocksService;
+        _finnhubService = finnhubService;
+        _configuration = configuration;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync(string? stockSymbol)
+    {
+        Dictionary<string, object>? profitDictionary = null;
+
+        if (stockSymbol != null)
+        {
+            profitDictionary = await _finnhubService.GetCompanyProfile(stockSymbol);
+            var stockPriceDictionary = await _finnhubService.GetStockPriceQuote(stockSymbol);
+            if (stockPriceDictionary != null && profitDictionary != null)
+                profitDictionary.Add("price", stockPriceDictionary["c"]);
+        }
+
+
+        if (profitDictionary != null && profitDictionary.ContainsKey("logo"))
+            return View(profitDictionary);
+        else
+            return Content("");
+    }
+}
