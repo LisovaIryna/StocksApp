609a785a22fbd9b85e2ce8efc142599dfbc03c51
LisovaIryna <79648928+LisovaIryna@users.noreply.github.com>
2025-10-18 20:41:48 +0300

    Add repositories and their interfaces to services and make some changes for future test rewriting

--- StocksApp/Program.cs
+++ StocksApp/Program.cs
@@ -1,5 +1,7 @@
 using Entities;
 using Microsoft.EntityFrameworkCore;
+using Repositories;
+using RepositoryContracts;
 using ServiceContracts;
 using ServiceContracts.DTO;
 using Services;
@@ -10,6 +12,8 @@ builder.Services.AddControllersWithViews();
 builder.Services.Configure<TradingOptions>(builder.Configuration.GetSection("TradingOptions"));
 builder.Services.AddTransient<IFinnhubService, FinnhubService>();
 builder.Services.AddTransient<IStocksService, StocksService>();
+builder.Services.AddTransient<IFinnhubRepository, FinnhubRepository>();
+builder.Services.AddTransient<IStocksRepository, StocksRepository>();
 builder.Services.AddDbContext<StockMarketDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
@@ -18,6 +22,10 @@ builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
+if (builder.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+
+if (builder.Environment.IsEnvironment("Test") == false)
 Rotativa.AspNetCore.RotativaConfiguration.Setup("wwwroot", wkhtmltopdfRelativePath: "Rotativa");
 
 app.UseStaticFiles();
@@ -25,3 +33,5 @@ app.UseRouting();
 app.MapControllers();
 
 app.Run();
+
+public partial class Program { } // make the auto-generated Program accessible programmatically
